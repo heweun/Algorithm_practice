52b6c0bef6a4fd4ee87d1e63bec547f459ffc2b8
heweun <109562023+heweun@users.noreply.github.com>
2023-02-16 10:23:12 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -1,10 +1,8 @@
 def solution(numbers):
-    num_dic = {i:0 for i in range(10)}
-    answer = -1
+    answer = 0
     
-    for num in numbers:
-        num_dic[num]+=1
-    
-    answer = sum(i for i in range(10) if num_dic[i]==0)
+    for i in range(10):
+        if i not in numbers:
+            answer += i
     
     return answer 
