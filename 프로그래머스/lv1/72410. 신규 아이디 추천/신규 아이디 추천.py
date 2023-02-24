bd8baa48df1c26f5773c2511f2d1e68f107bdc96
heweun <109562023+heweun@users.noreply.github.com>
2023-02-24 12:35:59 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,43 @@
+def solution(new_id):
+    answer = new_id
+    
+    #1. 대문자->소문자
+    answer = answer.lower()
+    
+    #2. "-","_","." 제외한 특수문자 제거
+    import string
+    symbols = string.punctuation.replace('-', '').replace('_', '').replace(".","")
+    
+    for symbol in symbols:
+        answer = answer.replace(symbol,"")
+    
+    #3. "." 2번이상 연속->. 한개로
+    while ".." in answer:
+        answer = answer.replace("..",".")
+        
+    #4. "." 처음이나 끝에 위치하면 제거
+    if answer[0] == ".":
+        answer = answer[1:]
+    elif answer[-1] == ".":
+        answer = answer[:-1]
+    
+    #5. 비어있으면->"a" 대입
+    if len(answer)==0:
+        answer = "a"
+    
+    #6.16자 이상->앞에서부터 15개만 남기고 삭제
+    if len(answer)>=16:
+        answer = answer[:15]
+    
+    #7. 마지막이 .이면 삭제
+    if answer[-1] == ".":
+        answer = answer[:-1]
+    
+    #8. 2자 이하면->마지막 문자 반복해서 3자까지 만들기
+    if len(answer)<=2:
+        while True:
+            answer += answer[-1]
+            if len(answer)>=3:
+                break
+        
+    return answer
