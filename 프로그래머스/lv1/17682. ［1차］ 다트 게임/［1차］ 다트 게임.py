c692858cfe5c817991ce7044eaeebc4ee1afdf84
heweun <109562023+heweun@users.noreply.github.com>
2023-03-18 11:04:03 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,21 +1,21 @@
 def solution(dartResult):
     answer = []
-    dartResult = dartResult.replace('10','k')
-    point = ['10' if i == 'k' else i for i in dartResult]
-    print(point)
-
-    i = -1
-    sdt = ['S', 'D', 'T']
-    for j in point:
-        if j in sdt :
-            answer[i] = answer[i] ** (sdt.index(j)+1)
-        elif j == '*':
-            answer[i] = answer[i] * 2
-            if i != 0 :
-                answer[i - 1] = answer[i - 1] * 2
-        elif j == '#':
-            answer[i] = answer[i] * (-1)
-        else:
-            answer.append(int(j))
+    score = ["S","D","T"]
+    dartResult = dartResult.replace("10", "A")
+    dart_list = ["10" if i=="A" else i for i in dartResult]
+    
+    i=-1
+    for d in dart_list:
+        if d.isnumeric() == True:
+            answer.append(int(d))
             i += 1
+        elif d in score:
+            answer[i] = answer[i]**(score.index(d)+1)
+        elif d == "#":
+            answer[i] = answer[i]*-1
+        else:
+            answer[i] = answer[i]*2
+            if i!=0:
+                answer[i-1] = answer[i-1]*2
+                
     return sum(answer)
