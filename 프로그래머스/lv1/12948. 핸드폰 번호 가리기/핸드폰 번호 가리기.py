bc2a0bc08969ceb34d5d7e4b4fd0865c3307c9e3
heweun <109562023+heweun@users.noreply.github.com>
2023-03-24 13:58:13 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.py
@@ -1,5 +1,5 @@
 import re
 
 def solution(phone_number):
-    answer = re.sub("[0-9]","*", phone_number[:-4])+phone_number[-4:]
+    answer = "*"*(len(phone_number)-4)+phone_number[-4:]
     return answer
