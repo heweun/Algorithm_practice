eb4e894ebe9755454f0ca5492cd4938b1ddc78f6
heweun <109562023+heweun@users.noreply.github.com>
2023-03-31 13:00:55 +0900

    [unrated] Title: 덧칠하기, Time: 2.07 ms, Memory: 12 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,9 @@
+def solution(n, m, section):
+    answer = painted = 0
+    
+    for i in section:
+        if i >= painted:
+            painted = i+m
+            answer += 1
+            
+    return answer
