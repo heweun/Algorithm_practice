176ed5047501f2168c49ef90e06c9519b475efcd
heweun <109562023+heweun@users.noreply.github.com>
2023-04-05 20:34:01 +0900

    [unrated] Title: 둘만의 암호, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -6,11 +6,5 @@ def solution(s, skip, index):
         alpha = alpha.replace(i,'')
     
     for j in s:
-        index_result = alpha.index(j)+index
-        if index_result>=len(alpha):
-            while index_result>0:
-                index_result -= len(alpha)
-            answer += alpha[index_result]
-        else:
-            answer += alpha[index_result]
+        answer += alpha[(alpha.index(j)+index)%len(alpha)]
     return answer
