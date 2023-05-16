ad56b4cb5e6706eb8277ef1a62a958ad86f4848d
heweun <109562023+heweun@users.noreply.github.com>
2023-05-16 21:11:05 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,35 +1,27 @@
-n, m = map(int, input().split())
-data = []
-result=[]
-# 입력한 데이터를 리스트에 저장
-for _ in range(n):
-    data.append(list(map(str, input())))
- 
-# 리스트를 8*8로 쪼개기
-for i in range(0, n-7):
-    for j in range(0, m-7):
+N,M,*a_list = open(0).read().split()
+result = []
+
+#리스트를 8*8로 쪼개기
+for i in range(int(N)-7): #N을 넘지 않는 수 중 가장 큰 수 까지
+    for j in range(int(M)-7): #M을 넘지 않는 수 중 가장 큰 수 까지
         count1 = 0
         count2 = 0
-        # 8*8 보드를 하나씩 검사하기
-        for x in range(i, i+8):
-            for y in range(j, j+8):
-                # 인덱스 합이 짝수인 경우 일정한 색 가지게 됨.
-                if (x+y) % 2 == 0:
-                    # 0,0 값이 B인 경우
-                    if data[x][y] != 'W':
-                        count1 += 1
-                    # 0,0 값이 W인 경우
-                    if data[x][y] != 'B':
-                        count2 += 1
-                # 인덱스 합이 홀수인 경우 일정한 색
+
+#쪼갠 8*8 검사하기
+        for x in range(i,i+8):
+            for y in range(j,j+8):
+                #인덱스의 합이 짝수->시작점과 같아야함
+                if (x+y)%2 == 0:
+                    if a_list[x][y]!= 'W': #W가 아니면
+                        count1 += 1 #W로 바꿔주고 +1
+                    else: #반대의 경우
+                        count2 += 1 #B로 바꿔주고 +1
+                #인덱스의 합이 홀수->시작점과 달라야함
                 else:
-                    # 0,0 값이 W인 경우
-                    if data[x][y] != 'W':
-                        count2 += 1
-                    # 0,0 값이 B인 경우
-                    if data[x][y] != 'B':
-                        count1 += 1
-        result.append(count1)
-        result.append(count2)
- 
+                    if a_list[x][y] != 'W': #W가 아니면
+                        count2 += 1 #B로 바꿔주고 +1
+                    else: #B가 아니면
+                        count1 += 1 #W로 바꿔주고 +1
+        result.append(count1) #W로 시작할때 경우의 수
+        result.append(count2) #B로 시작할때 경우의 수
 print(min(result))
