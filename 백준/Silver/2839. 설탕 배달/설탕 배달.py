b9dd04431a20347a0f0fe2de18ec26f5d146f0c6
heweun <109562023+heweun@users.noreply.github.com>
2023-05-17 11:50:30 +0900

    [Silver IV] Title: 설탕 배달, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,12 +1,14 @@
-sugar = int(input())
+N = int(input())
+count = 0
 
-bag = 0
-while sugar >= 0 :
-    if sugar % 5 == 0 :  # 5의 배수이면
-        bag += (sugar // 5)  # 5로 나눈 몫을 구해야 정수가 됨
-        print(bag)
-        break
-    sugar -= 3  
-    bag += 1  # 5의 배수가 될 때까지 설탕-3, 봉지+1
-else :
-    print(-1)
+while N>=0:
+  if N%5 == 0:
+    count += N//5
+    print(count)
+    break
+
+  N -= 3
+  count += 1
+  
+else:
+  print(-1)
