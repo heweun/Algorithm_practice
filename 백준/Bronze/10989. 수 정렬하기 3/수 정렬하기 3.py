95446da385b4f332310fdff131495341931f094a
heweun <109562023+heweun@users.noreply.github.com>
2023-05-25 10:46:15 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9364 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,15 +1,12 @@
 import sys
 
-N = int(input())
-
-check_list = [0] * 10001
-
+N = int(sys.stdin.readline()) #개수
+li = [0]*10001 #딱 범위까지만 만들어지도록 제한
 for i in range(N):
-    input_num = int(sys.stdin.readline())
-    
-    check_list[input_num] = check_list[input_num] + 1
-    
-for i in range(10001):
-    if check_list[i] != 0:
-        for j in range(check_list[i]):
-            print(i)
+    a = sys.stdin.readline()
+    li[int(a)] += 1 #a번째 자리에 몇번 나왔는지 값 채워 넣기
+
+for j in range(1,10001): #범위 안에서
+    if li[j] >= 1: #a가 1번 이상 나왔으면 
+        for k in range(li[j]): #나온 개수만큼
+            print(j) #a 출력
