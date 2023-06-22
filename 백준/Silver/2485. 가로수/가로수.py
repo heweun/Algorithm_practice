8e8e4781579a223626331690931fe3c9a78886e7
heweun <109562023+heweun@users.noreply.github.com>
2023-06-22 17:41:50 +0900

    [Silver IV] Title: 가로수, Time: 76 ms, Memory: 44296 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.py
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -2,19 +2,12 @@ from math import gcd #최대 공약수 찾는 함수
 
 n, *n_list = map(int, open(0).read().split())
 
-len_n = []
-for i,j in zip(n_list[:-1],n_list[1:]):
-    len_n.append(j-i) #set은 add랑 짝꿍, 인덱싱 불가
+#두 지점 사이의 거리
+len_n = [j-i for i,j in zip(n_list[:-1],n_list[1:])]
 
-#리스트에 들어있는 수들의 최대공약수 구하기
-g = len_n[0] #첫번째 값부터 넣어서 차례차례 비교
-for j in range(1, len(len_n)):
-    g = gcd(g, len_n[j]) #최대공약수 == g
+#모든 원소의 최대공약수 한번에 구할 수 있음
+g = gcd(*len_n)
 
 #가로수 개수 구하기: 두 나무 사이의 거리를 최대 공약수로 나누고 -1(이미 있으니까)
-answer = 0
-for each in len_n:
-    answer += each//g-1
-
+answer = sum([each//g-1 for each in len_n])
 print(answer)
-
