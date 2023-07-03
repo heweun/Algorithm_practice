e749027860c7c5267f65d519daedfc154b45b33a
heweun <109562023+heweun@users.noreply.github.com>
2023-07-03 10:19:46 +0900

    [Silver V] Title: 다리 놓기, Time: 80 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -3,5 +3,5 @@ import math
 N = int(input())
 
 for _ in range(N):
-  A = list(map(int, input().split()))
-  print(math.comb(max(A),min(A)))
+  a,b = map(int, input().split())
+  print(math.comb(b,a))
