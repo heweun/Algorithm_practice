124f6ed174a6669cfd5cb6560643a013b921c644
heweun <109562023+heweun@users.noreply.github.com>
2023-07-05 09:59:47 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 72 ms, Memory: 44684 KB -BaekjoonHub

--- 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.py
@@ -1,24 +1,8 @@
-import sys
+N = open(0).read().split('ENTER')
+#print(N)
 
-input = sys.stdin.readline
-"""
-ENTER이후 중복이 없는 자료구조에 저장 (set)
-ENTER를 입력받을 때 마다 ENTER를 초기화
-  +) set에 있는 개수를 ans에 추가.
-"""
-
-N = int(input())
-s = set()
-ans = 0
-input()
-
-for i in range(N - 1):
-  user = input().strip()
-  if user == "ENTER":
-    ans += len(s)
-    s.clear()
-  else:
-    s.add(user)
-ans += len(s)
-
-print(ans)
+answer = 0
+for n in N:
+    #print(n.split())
+    answer += len(set(n.split()))
+print(answer-1)
