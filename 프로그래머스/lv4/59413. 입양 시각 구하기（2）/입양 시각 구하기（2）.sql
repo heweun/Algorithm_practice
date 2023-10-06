f874c95b129c9a714ac873f19494fe57c3f02e6f
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2023-10-06 15:06:51 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++ 프로그래머스/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -1,17 +1,14 @@
 -- 코드를 입력하세요
-#시간대가 7~19시 밖에 없음-->없는 시간대를 만들어 줘야함
-WITH RECURSIVE blank AS ( #빈 가상의 테이블 만들기
-    SELECT 0 AS HOUR #select 0 as hour-> 0이 데이터인 hour변수 생성
-    UNION ALL #데이터 0~23으로 늘리기
-    SELECT HOUR + 1 #hour(0이 시작)+1
-      FROM blank 
-     WHERE HOUR < 23 #hour이 22가 될때 까지
-)
+WITH RECURSIVE BLANK AS (SELECT 0 AS HOUR
+                        UNION ALL
+                        SELECT HOUR + 1
+                        FROM BLANK
+                        WHERE HOUR<23)
 
-#select * from blank #가상의 테이블 출력
+#SELECT * FROM BLANK
 
-select hour, count(hour(a.datetime)) as count
-from blank b
-left join animal_outs a on b.hour = hour(a.datetime)
-group by b.hour
-order by b.hour
+SELECT HOUR, COUNT(HOUR(A.DATETIME)) AS COUNT
+FROM BLANK B
+LEFT JOIN ANIMAL_OUTS A ON B.HOUR = HOUR(A.DATETIME)
+GROUP BY HOUR
+ORDER BY HOUR
