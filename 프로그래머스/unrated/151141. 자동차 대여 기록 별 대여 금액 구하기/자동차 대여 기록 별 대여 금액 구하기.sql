23e0d6b6b799730e636f4dbbd70c3bbf01435f23
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2023-10-06 23:03:44 +0900

    [unrated] Title: 자동차 대여 기록 별 대여 금액 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
+++ 프로그래머스/unrated/151141. 자동차 대여 기록 별 대여 금액 구하기/자동차 대여 기록 별 대여 금액 구하기.sql
@@ -1,22 +1,21 @@
-WITH value AS (
-    SELECT car.daily_fee, car.car_type, his.history_id,
-           DATEDIFF(end_date, start_date) + 1 AS period,
-    CASE 
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 30 THEN '30일 이상'
-      WHEN DATEDIFF(end_date, start_date) + 1 >= 7 THEN '7일 이상'
-      ELSE 'NONE' END AS duration_type
-FROM car_rental_company_rental_history AS his
-INNER JOIN car_rental_company_car AS car ON car.car_id = his.car_id
-WHERE car.car_type = '트럭')   
+#임시 테이블 만들기
+WITH DISCOUNT AS(SELECT A.DAILY_FEE, A.CAR_TYPE, B.HISTORY_ID,
+                 DATEDIFF(END_DATE, START_DATE)+1 AS PERIOD,
+                 CASE
+                 WHEN DATEDIFF(END_DATE, START_DATE)+1>=90 THEN '90일 이상'
+                 WHEN DATEDIFF(END_DATE, START_DATE)+1>=30 THEN '30일 이상'
+                 WHEN DATEDIFF(END_DATE, START_DATE)+1>=7 THEN '7일 이상'
+                 ELSE 'NONE'
+                 END AS duration_type
+                 
+                FROM CAR_RENTAL_COMPANY_CAR A
+                JOIN CAR_RENTAL_COMPANY_RENTAL_HISTORY B ON A.CAR_ID=B.CAR_ID
+                WHERE A.CAR_TYPE = '트럭')
 
-
-
-SELECT value.history_id, 
-    ROUND(value.daily_fee * value.period * 
-          (100 - IFNULL(plan.discount_rate,0)) / 100) AS FEE
-FROM value
-LEFT JOIN car_rental_company_discount_plan AS plan 
-    ON plan.duration_type = value.duration_type 
-    AND plan.car_type = value.car_type
-ORDER BY 2 DESC, 1 DESC
+#임시 테이블과 새로 JOIN하기
+SELECT D.HISTORY_ID,
+ROUND(DAILY_FEE * PERIOD * (1 - IFNULL(discount_rate,0) * 0.01), 0) AS FEE
+FROM DISCOUNT D
+LEFT JOIN CAR_RENTAL_COMPANY_DISCOUNT_PLAN C ON D.CAR_TYPE = C.CAR_TYPE
+AND D.duration_type = C.duration_type
+ORDER BY FEE DESC, D.HISTORY_ID DESC
