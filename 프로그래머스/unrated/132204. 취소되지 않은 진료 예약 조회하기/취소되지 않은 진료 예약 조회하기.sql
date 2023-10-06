1b41b5f32563aca4c6c803add68d262cd2c343f9
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2023-10-06 19:30:14 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -1,14 +1,9 @@
 -- 코드를 입력하세요
-#PATIENT 환자번호, 환자이름, 성별코드, 나이, 전화번호
-#DOCTOR 의사이름, 의사ID, 면허번호, 고용일, 진료과코드, 전화번호
-#APPOINTMENT 진료예약일시, 진료예약번호, 환자번호, 진료과코드, 의사ID, 예약취소여부, 예약취소 날짜
-SELECT APNT_NO, B.PT_NAME, A.PT_NO, A.MCDP_CD, C.DR_NAME, APNT_YMD
-from APPOINTMENT A
-join PATIENT B on A.PT_NO = B.PT_NO
-join DOCTOR C on A.MDDR_ID = C.DR_ID
-
-where date_format(APNT_YMD,'%Y-%m-%d') = '2022-04-13'
-and APNT_CNCL_YN != "Y"
-and A.MCDP_CD = 'CS'
-
-order by APNT_YMD
+SELECT C.APNT_NO, A.PT_NAME, A.PT_NO, C.MCDP_CD, B.DR_NAME, C.APNT_YMD
+FROM APPOINTMENT C
+JOIN PATIENT A ON A.PT_NO = C.PT_NO
+JOIN DOCTOR B ON B.DR_ID = C.MDDR_ID
+WHERE C.APNT_CNCL_YN = 'N'
+AND C.MCDP_CD = 'CS'
+AND C.APNT_YMD LIKE '2022-04-13%'
+ORDER BY C.APNT_YMD
