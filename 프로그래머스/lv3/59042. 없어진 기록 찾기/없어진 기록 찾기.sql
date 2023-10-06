bc95bf8566a49aa61186f6c07fb1cc45ccff6f05
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2023-10-06 16:16:59 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/lv3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
-SELECT A.animal_id, A.name
+SELECT A.ANIMAL_ID, A.NAME
 from ANIMAL_OUTS A
-left join ANIMAL_INS B on A.animal_id = B.animal_id
-where B.animal_id is null
+left join ANIMAL_INS B on A.ANIMAL_ID = B.ANIMAL_ID
+where B.ANIMAL_ID is null
