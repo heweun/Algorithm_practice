85cd572197e5dcdc5d626541f1e207963eb8d61b
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-01-26 18:46:30 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 8.26 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -26,52 +26,54 @@
    
         
 #     return answer
+
 def solution(friends, gifts):
     answer = 0
     length = len(friends)
     score = [0] * len(friends)
 
     #0. Create Table
-    give = [[0] * length for _ in range(length)] # 선물은 준 횟수
+    give = [[0] * length for _ in range(length)] # 인원수별 선물 확인할 이중리스트 제작
     for gift in gifts:
         gift = gift.split(" ")
         giver = friends.index(gift[0])
         getter = friends.index(gift[1])
 
-        give[giver][getter] += 1
+        give[giver][getter] += 1 #누구에게 줬는지 확인하며 +1
+    # print(f'final give:{give}') #선물 주고 받은 리스트
 
-    #1. Calculation
     for i in range(length):
         for j in range(i + 1, length):
+            # print(f'i:{i}, j:{j}')
             give_score = give[i][j] # i가 j에게 준 선물의 개수
             get_score = give[j][i] #j가 i에게 준 선물의 개수
+            # print(f'give_score:{give_score}, get_score:{get_score}')
 
-            #1-1. 선물을 주고 받은 경우
+            #give와 get비교하기
             if (give_score != get_score) and (give_score > 0 or get_score > 0):
+                #둘이 주고 받은게 다르고, 0보다 클때
                 if give_score > get_score:
-                    score[i] += 1
+                    score[i] += 1 #giver가 받음
 
                 else:
-                    score[j] += 1
+                    score[j] += 1 #getter가 받음
 
-            #1-2. 선물 지수 계산
+            #0이거나, 동점일때 선물 지수 확인
             else:
+                # print(f'give[{i}]:{give[i]} give[{j}]:{give[j]}')
                 giver_present_score = sum(give[i])
                 getter_present_score = sum(give[j])
-
+                
+                # print(f'giver_present_score:{giver_present_score}\ngetter_present_score:{getter_present_score}')
                 for k in range(length):
                     giver_present_score -= give[k][i]
                     getter_present_score -= give[k][j]
-
 
                 if giver_present_score > getter_present_score:
                     score[i] += 1
 
                 elif giver_present_score < getter_present_score:
                     score[j] += 1
-
-
-
 
     answer = max(score)
     return answer
