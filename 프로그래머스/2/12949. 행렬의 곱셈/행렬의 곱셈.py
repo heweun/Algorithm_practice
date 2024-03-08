fadb5855f581379a499e9a50794674c3deeecff7
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-03-08 13:19:46 +0900

    [level 2] Title: 행렬의 곱셈, Time: 0.58 ms, Memory: 29 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.py
@@ -1,7 +1,25 @@
 import numpy as np
 
 def solution(arr1, arr2):
+    
     arr1 = np.array(arr1)
     arr2 = np.array(arr2)
-    # print(arr1.dot(arr2))
+    
     return arr1.dot(arr2).tolist()
+    
+    # answer = []
+    # for A_row in arr1:
+    #     # print(f'A_row:{A_row}')
+    #     arr = []
+    #     for B_col in zip(*arr2):
+    #         # print(f'B_col:{B_col}')
+    #         tmp = 0
+    #         for a,b in zip(A_row, B_col):
+    #             # print(a*b)
+    #             tmp += a*b
+    #         arr.append(tmp)
+    #     answer.append(arr)
+    
+    # print(f'answer:{answer}')    
+    
+   
