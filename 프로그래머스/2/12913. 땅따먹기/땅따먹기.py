032e16f7f5b6609801db2e4e53083d7cfaee3ebd
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-04-21 14:23:44 +0900

    [level 2] Title: 땅따먹기, Time: 213.24 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.py
@@ -1,11 +1,7 @@
 def solution(land):
-    answer = 0
-    
     for i in range(1,len(land)):
         for j in range(len(land[0])):
-            # print(f'i:{i} j:{j} land:{land[i][j]}')
-            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:])
-            
-    # print(f'land:{land}')
-    answer = max(land[-1])
-    return answer
+            # print(f'i:{i} j:{j}')
+            land[i][j] += max(land[i-1][j+1:]+land[i-1][:j])
+
+    return max(land[-1])
