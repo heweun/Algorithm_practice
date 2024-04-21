15ca5d17b8f0849bd28afd062fdefa2bb20900f5
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-04-21 15:37:53 +0900

    [level 2] Title: 롤케이크 자르기, Time: 561.97 ms, Memory: 51.4 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,15 +1,16 @@
 from collections import Counter
 
 def solution(topping):
-    dic = Counter(topping)
-    print(f'dic:{dic}')
-    set_dic = set()
     answer = 0
+    dic = Counter(topping) #숫자를 세어 두기
+    set_dic = set()
+    
     for i in topping:
-        dic[i] -= 1
-        set_dic.add(i)
-        if dic[i] == 0:
-            dic.pop(i)
-        if len(dic) == len(set_dic):
-            answer += 1
+        dic[i] -= 1 #한번 셋으니까 개수 줄이고
+        set_dic.add(i) #새로운 곳에 넣기(왼쪽이라고 생각)
+        if dic[i] == 0: #해당 토핑을 이미 새로운 곳에 다 넣었다면
+            dic.pop(i) #딕셔너리에서 지워주기
+        if len(dic) == len(set_dic): #왼쪽 오른쪽(딕셔너리) 길이가 같아지면
+            answer += 1 #성공개수로 추가
+    
     return answer
