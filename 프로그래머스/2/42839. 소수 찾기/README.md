68d0894123bba7b059c0b93eeab7f9ea9145c73f
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-05-11 21:10:41 +0900

    [level 2] Title: 소수 찾기, Time: 0.20 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.23 ms
+메모리: 10.4 MB, 시간: 0.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 16:23:17
+2024년 05월 11일 21:10:40
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -1,21 +1,22 @@
 from itertools import permutations
 
 def sosu(num):
-    if num <= 1:
+    if num<=1:
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2,int(num**0.5)+1):
+        if num%i == 0:
             return False
     return True
 
 def solution(numbers):
     answer = 0
-    b_list = set()
-    for a in range(1,len(numbers)+1):
-       for p in permutations(numbers,a):
-        b_list.add(int("".join(p)))
-    # print(b_list)
-    for b in b_list:
-        if sosu(b):
+    sosu_set = set()
+    for i in range(1,len(numbers)+1):
+        for p in permutations(numbers,i):
+            sosu_set.add(int("".join(p)))
+    # print(f'sosu_set:{sosu_set}')
+    for s in sosu_set:
+        if sosu(s):
+            print(s)
             answer += 1
     return answer
