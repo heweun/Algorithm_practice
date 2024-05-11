0f80bde24ccd3ebf637d568cc02dbc8051b463cc
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-05-11 21:01:00 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746#) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.02 ms
+메모리: 10.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 09일 15:51:59
+2024년 05월 11일 21:00:59
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,8 +1,8 @@
-#numbers의 원소는 0 이상 1,000 이하입니다. 크으..3자리까지만
-
 def solution(numbers):
-    numbers = list(map(str,numbers))
-    numbers.sort(key = lambda x:x*3, reverse=True)
+    answer = ''
+    numbers = list(map(str, numbers))
+    numbers.sort(key=lambda x:x*3, reverse=True)
+    # print(f'numbers:{numbers}')
     
-    answer = "".join(numbers)
-    return str(int(answer))
+    answer = str(int("".join(numbers)))
+    return answer
