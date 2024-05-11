b4b4d288a4e6d084f27a2005ab94d03e435c1ea9
Hweeun Kwon <109562023+heweun@users.noreply.github.com>
2024-05-11 20:57:27 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -1,21 +1,17 @@
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    bridge = [0]*bridge_length #총 올라갈 수 있는 자리 설정
-    # print(f'start bridge:{bridge}')
+    bridge = [0]*bridge_length
+    #print(f'bridge:{bridge}')
     
-    curweight = 0 #다리 위 무게 측정
+    current_weight = 0
     while truck_weights:
-        answer += 1 #시작부터 무조건 1초
-        curweight = curweight-bridge.pop(0)
-        
-        # print(f"curweight: {curweight}, truck_weights: {truck_weights}")
-        
-        if curweight + truck_weights[0] <= weight:
-            curweight += truck_weights[0]
+        #print(f'current_weight:{current_weight}, bridge:{bridge}')
+        answer += 1
+        current_weight -= bridge.pop(0)
+        if current_weight+truck_weights[0] <= weight:
+            current_weight+=truck_weights[0]
             bridge.append(truck_weights.pop(0))
         else:
-            bridge.append(0) #자리수 채워주기
-        
-        # print(f'bridge:{bridge}')
-    answer += bridge_length
+            bridge.append(0)
+    answer += len(bridge)
     return answer
